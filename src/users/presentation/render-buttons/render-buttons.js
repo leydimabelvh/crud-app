2172610c289cd4f885bcc39bcf2c2435fbc3d608
Leydi Velazco <leydi.velazco@outlook.es>
2023-02-19 13:14:19 -0500

    Add function that renders buttons and current page

---
+++ src/users/presentation/render-buttons/render-buttons.js
@@ -0,0 +1,31 @@
+import usersStore from '../../store/users-store';
+import { renderTable } from '../render-table/render-table';
+import './render-buttons.css';
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderButtons = ( element ) => {
+
+    let currentPage = usersStore.getCurrentPage();
+
+    const nextButton = document.createElement( 'button' );
+    nextButton.innerText = 'Next >';
+    
+    const previousButton = document.createElement( 'button' );
+    previousButton.innerText = '< Previous';
+
+    const currentPageLabel = document.createElement( 'span' );
+    currentPageLabel.id = 'currentPage';
+    currentPageLabel.innerText = currentPage;
+
+    element.append( previousButton, currentPageLabel, nextButton );
+
+    nextButton.addEventListener( 'click', () => {
+        usersStore.loadNextPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable(); 
+    } ); 
+
+}
