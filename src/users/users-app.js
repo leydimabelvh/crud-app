08fa72d4b99fff3e33efeecfd0287fc0596d4225
Leydi Velazco <leydi.velazco@outlook.es>
2023-02-19 14:16:32 -0500

    Call functions required for users component

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,3 +1,5 @@
+import { renderButtons } from './presentation/render-buttons/render-buttons';
+import { renderTable } from './presentation/render-table/render-table';
 import usersStore from './store/users-store';
 
 /**
@@ -8,6 +10,11 @@ export const UsersApp = async( element ) => {
     document.querySelector('#title-app').innerHTML = 'CRUD App';
 
     element.innerHTML = 'Loading...';
+    
+    await usersStore.loadNextPage();
+    
+    element.innerHTML = '';
 
-    usersStore.loadNextPage();
+    renderTable( element );
+    renderButtons( element );
 }
