09096aba3bd120d8ef8439c201c1aede4a2bcfcf
Leydi Velazco <leydi.velazco@outlook.es>
2023-02-19 10:17:44 -0500

    Add load-user-by-page module

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -1,12 +1,17 @@
+import { localhostUserToModel } from '../mappers/localhost-user.mapper';
+import { User } from '../models/user';
+
 /**
  * 
  * @param {Number} page 
- * @returns
+ * @returns { Promise<User[]>}
  */
 export const loadUsersByPage = async( page = 1 ) => {
     const url = `${ import.meta.env.VITE_BASE_URL }/users?_page=${ page }`;
     const response = await fetch( url );
     const data = await response.json() ;
 
-    console.log(data);
+    const userList = data.map( userLike => localhostUserToModel( userLike ) ) ; 
+
+    return userList;
 }
