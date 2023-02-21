8a339318a5e64175e8ed6c32511deba468dcb2fb
Leydi Velazco <leydi.velazco@outlook.es>
2023-02-21 12:23:45 -0500

    Add listener that prevents default behavior of submit button

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -5,24 +5,11 @@ let modalContainer;
 
 export const showModal = () => {
     modalContainer?.classList.remove( 'hide-modal' );
-
-    // const showModal = document.querySelector( '.modal-container');
-    // showModal.classList.remove( 'hide-modal' );
 }
 
 export const hideModal = () => {
     modalContainer?.classList.add( 'hide-modal' );
 }
-
-
-
-
-
-
-
-
-
-
 
 /**
  * 
@@ -37,19 +24,18 @@ export const renderModal = ( element ) => {
 
     element.append( modalContainer );
 
-    const modalButton = document.querySelector( '.modal-form-button' );
+    const modalForm  = document.querySelector( '.modal-form' );
 
     modalContainer.addEventListener( 'click', ( event ) => {
         const isModal = event.target.matches( '.modal-container' );
 
         if ( !isModal ) return;
         hideModal();
-        // const hideModal = document.querySelector( '.modal-container' );
-        // hideModal.classList.add( 'hide-modal' );
+        
     } );
 
-    modalButton.addEventListener( 'click', () => {
-        const hideModal = document.querySelector( '.modal-container' );
-        hideModal.classList.add( 'hide-modal' );
+    modalForm.addEventListener( 'submit', (event) => {
+        event.preventDefault();
     } );
+
 }
